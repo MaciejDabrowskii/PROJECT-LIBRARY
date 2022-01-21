b920419b64a8e8781e49a8a33293cc1edf2c1738
Jimmy <dabrowski.mac.1@gmail.com>
2022-01-21 14:03:08 +0100

    some js

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.getElementById("btn");
+
+
+button.addEventListener("click", ()=> {
+    let title = document.getElementById("inputTitle").value;
+    let author = document.getElementById("inputAuthor").value;
+    let pagesNumber = document.getElementById("inputNumber").value;
+
+});
