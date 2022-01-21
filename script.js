74858ac7c6198e499c482afca3ffa7baf0e06c74
Jimmy <dabrowski.mac.1@gmail.com>
2022-01-22 00:48:16 +0100

    changed concept of adding books to modal popup

--- script.js
+++ script.js
@@ -1,32 +1,50 @@
 // let button = document.getElementById("btn");
 let myLibrary = [];
-const openBtn = document.getElementById("open");
-const overlay = document.getElementById("overlay")
+const openButton = document.getElementById("open");
+const overlay = document.getElementById("overlay");
+const closeButton = document.getElementById('close');
+const addButton = document.getElementById("addToLibrary");
 
-openBtn.addEventListener('click', function() {
-    const modal = document.getElementById('modal');
-    modal.classList.add('active');
-    overlay.classList.add('active');
+openButton.addEventListener('click', function() {
+        const modal = document.getElementById('modal');
+        modal.classList.add('active');
+        overlay.classList.add('active');
     }
 );
 
-// button.addEventListener("click", ()=> {
-//     let title = document.getElementById("inputTitle").value;
-//     let author = document.getElementById("inputAuthor").value;
-//     let pagesNumber = document.getElementById("inputNumber").value;
-//     const book = new Book(title, author, pagesNumber)
-//     myLibrary.push(book);
-// });
+closeButton.addEventListener('click', () => {
+        const modal = document.getElementById('modal');
+        const title = document.getElementById("inputTitle");
+        const author = document.getElementById("inputAuthor");
+        const pagesNumber = document.getElementById("inputNumber");
+        reset(title, author, pagesNumber);
+    }
+);
 
+addButton.addEventListener("click", ()=> {
+        const title = document.getElementById("inputTitle");
+        const author = document.getElementById("inputAuthor");
+        const pagesNumber = document.getElementById("inputNumber");
+        const modal = document.getElementById('modal');
+        const book = new Book(title.value, author.value, pagesNumber.value)
+        myLibrary.push(book);
+        reset(title, author, pagesNumber);
 
+    }
+);
 
+function reset(title, author, pagesNumber) {
+    title.value = "";
+    author.value = "";
+    pagesNumber.value = "";
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
 
-// function Book(title, author, pagesNumber, alreadyRead) {
-//     this.title = title
-//     this.author = author
-//     this.pagesNumber = pagesNumber
-//     this.alreadyRead = alreadyRead
-//     this.info = function() {
-//      return (`${title} by ${author}, ${pagesNumber} pages, ${alreadyRead}.`)
-//     }
-//   }
+function Book(title, author, pagesNumber, alreadyRead) {
+        this.title = title
+        this.author = author
+        this.pagesNumber = pagesNumber
+        this.alreadyRead = alreadyRead
+        this.info = () => (`${title} by ${author}, ${pagesNumber} pages, ${alreadyRead}.`)
+}
