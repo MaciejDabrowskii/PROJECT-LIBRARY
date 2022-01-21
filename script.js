74a8ad21edbbac51b2dd28368342a91c8a98d31d
Jimmy <dabrowski.mac.1@gmail.com>
2022-01-21 22:12:20 +0100

    changed concept of adding books to modal popup

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 let button = document.getElementById("btn");
+let myLibrary = [];
+
 
 
 button.addEventListener("click", ()=> {
     let title = document.getElementById("inputTitle").value;
     let author = document.getElementById("inputAuthor").value;
     let pagesNumber = document.getElementById("inputNumber").value;
-
+    const book = new Book(title, author, pagesNumber)
+    myLibrary.push(book);
 });
+
+
+
+
+function Book(title, author, pagesNumber, alreadyRead) {
+    this.title = title
+    this.author = author
+    this.pagesNumber = pagesNumber
+    this.alreadyRead = alreadyRead
+    this.info = function() {
+     return (`${title} by ${author}, ${pagesNumber} pages, ${alreadyRead}.`)
+    }
+  }
